17df74b4464dd7f013854a5395130b9e402d683e
Wladimir J. van der Laan <laanwj@gmail.com>
2017-08-22 09:31:27 +0200

    Merge #11058: Comments: More comments on functions/globals in standard.h.
    360b464 Comments: More comments on functions/globals in standard.h. (Jim Posen)
    
    Pull request description:
    
      I was confused about what "data carrier" meant, so I wanted to comment the `fAcceptDatacarrier` and `nMaxDatacarrierBytes` fields specifically. Then I figured I'd add docs for the rest of the functions.
    
    Tree-SHA512: e6d0cfe6f4a2ab52ae76f984b1f5d8de371ae938e7832be8b02517d868f1caea62fec8888c917a2bd3d8ef74025de7f00dc96923fa56436dc6b190626652bf29
    
    remove witness
    
    Signed-off-by: Pasta <pasta@dashboost.org>

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -35,9 +35,6 @@ const char* GetTxnOutputType(txnouttype t)
     return nullptr;
 }
 
-/**
- * Return public keys or hashes from scriptPubKey, for 'standard' transaction types.
- */
 bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::vector<unsigned char> >& vSolutionsRet)
 {
     // Templates
