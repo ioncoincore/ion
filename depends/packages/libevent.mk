9e14999dccf3986ecf58b19a71b7dd55642298b4
MarcoFalke <falke.marco@gmail.com>
2017-09-10 04:38:59 +0200

    Merge #11286: [depends] Don't build libevent sample code
    f38c05104 [depends] Don't build libevent sample code (fanquake)
    
    Pull request description:
    
      Noticed today while doing Windows depends builds.
      Before and after libevent build on OS X.
    
      ```
      Building libevent...
        GEN      include/event2/event-config.h
      /Applications/Xcode.app/Contents/Developer/usr/bin/make  all-am
        CC       buffer.lo
        CC       bufferevent.lo
        CC       bufferevent_filter.lo
        CC       bufferevent_ratelim.lo
        CC       bufferevent_sock.lo
        CC       bufferevent_pair.lo
        CC       event.lo
        CC       evmap.lo
        CC       evthread.lo
        CC       evutil.lo
        CC       evutil_rand.lo
        CC       evutil_time.lo
        CC       listener.lo
        CC       log.lo
        CC       select.lo
        CC       poll.lo
        CC       kqueue.lo
        CC       signal.lo
        CC       evdns.lo
        CC       event_tagging.lo
        CC       evrpc.lo
        CC       http.lo
        CC       evthread_pthread.lo
        CC       sample/dns-example.o
        CC       sample/event-read-fifo.o
        CC       sample/hello-world.o
        CC       sample/http-server.o
        CC       sample/http-connect.o
        CC       sample/signal-test.o
        CC       sample/time-test.o
        CCLD     libevent_core.la
        CCLD     libevent_pthreads.la
        CCLD     libevent.la
        CCLD     libevent_extra.la
        CCLD     sample/event-read-fifo
        CCLD     sample/dns-example
        CCLD     sample/hello-world
        CCLD     sample/http-server
        CCLD     sample/http-connect
        CCLD     sample/signal-test
        CCLD     sample/time-test
      Staging libevent...
      ```
    
      ```
      Building libevent...
        GEN      include/event2/event-config.h
      /Applications/Xcode.app/Contents/Developer/usr/bin/make  all-am
        CC       buffer.lo
        CC       bufferevent.lo
        CC       bufferevent_filter.lo
        CC       bufferevent_pair.lo
        CC       bufferevent_ratelim.lo
        CC       bufferevent_sock.lo
        CC       event.lo
        CC       evmap.lo
        CC       evthread.lo
        CC       evutil.lo
        CC       evutil_rand.lo
        CC       evutil_time.lo
        CC       listener.lo
        CC       log.lo
        CC       select.lo
        CC       poll.lo
        CC       kqueue.lo
        CC       signal.lo
        CC       evdns.lo
        CC       event_tagging.lo
        CC       evrpc.lo
        CC       http.lo
        CC       evthread_pthread.lo
        CCLD     libevent_core.la
        CCLD     libevent_pthreads.la
        CCLD     libevent.la
        CCLD     libevent_extra.la
      Staging libevent...
      ```
    
    Tree-SHA512: a82bde38e110a16f934c0f1e95d3ae66662c1b3c1367e4a6e7c4994f585ebeea4f5cbee4d98a8dc64a8e8936eb9f4bb0c55fb65e718f724d88e81cb8fa84be88

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -10,7 +10,6 @@ endef
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-samples
-  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_android=--with-pic
