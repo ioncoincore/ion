a6a5bfc7e058cbd0687e0004d3b1433f6537d7d0
Wladimir J. van der Laan <laanwj@gmail.com>
2017-10-11 11:25:41 +0200

    Merge #10941: Add blocknotify and walletnotify functional tests
    857b32b [tests] Add -walletnotify functional test (João Barbosa)
    df18d29 [tests] Add -blocknotify functional test (João Barbosa)
    9c72a46 [tests] Tidy up forknotify.py (John Newbery)
    
    Pull request description:
    
      This patch adds the missing functional tests for `-blocknotify` and `-walletnotify` notifications. The `-alertnotify` test file `forknotify.py` is renamed to `notifications.py` to accommodate the new tests. Credits to @jnewbery for this cleanup and unification.
    
    Tree-SHA512: ee7b28b9f1bd225587efaefa6427c6d080ccb50ead390b23d94717c137a317183b37be00da0b2bffedd4192a363c971dea510d78d29278eb9fa76374f7855c09

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -166,7 +166,7 @@ EXTENDED_SCRIPTS = [
     #'txn_doublespend.py', # not working TODO fix it
     #'txn_clone.py --mineblock', # not working TODO fix it
     'txindex.py',
-    'forknotify.py',
+    'notifications.py',
     'invalidateblock.py',
 ]
 
