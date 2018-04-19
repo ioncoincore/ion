40977a0c5e8650e81f56641e5fed1470bb9dc31f
John Newbery <john@johnnewbery.com>
2018-04-19 17:42:40 -0400

    Merge #10740: [wallet] `loadwallet` RPC - load wallet at runtime
    cd53981 [docs] Add release notes for `loadwallet` RPC. (John Newbery)
    a46aeb6 [wallet] [tests] Test loadwallet (John Newbery)
    5d15260 [wallet] [rpc] Add loadwallet RPC (John Newbery)
    876eb64 [wallet] Pass error message back from CWallet::Verify() (John Newbery)
    e0e90db [wallet] Add CWallet::Verify function (John Newbery)
    470316c [wallet] setup wallet background flushing in WalletInit directly (John Newbery)
    59b87a2 [wallet] Fix potential memory leak in CreateWalletFromFile (John Newbery)
    
    Pull request description:
    
      Adds a `loadwallet` RPCs. This allows wallets to be loaded dynamically during runtime without having to stop-start the node with new `-wallet` params.
    
      Includes functional tests and release notes.
    
      Limitations:
    
      - currently this functionality is only available through the RPC interface.
      - wallets loaded in this way will not be displayed in the GUI.
    
    Tree-SHA512: f80dfe32b77f5c97ea3732ac538de7d6ed7e7cd0413c2ec91096bb652ad9bccf05d847ddbe81e7cd3cd44eb8030a51a5f00083871228b1b9b0b8398994f6f9f1

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -87,7 +87,6 @@ class COutput;
 class CReserveKey;
 class CReward;
 class CScript;
-class CScheduler;
 class CTxMemPool;
 class CBlockPolicyEstimator;
 class CWalletTx;
@@ -751,7 +750,6 @@ class WalletRescanReserver; //forward declarations for ScanForWalletTransactions
 class CWallet final : public CCryptoKeyStore, public CValidationInterface
 {
 private:
-    static std::atomic<bool> fFlushScheduled;
     std::atomic<bool> fAbortRescan;
     std::atomic<bool> fScanningWallet; //controlled by WalletRescanReserver
     std::mutex mutexScanning;
@@ -1261,6 +1259,9 @@ public:
     /* Mark a transaction (and it in-wallet descendants) as abandoned so its inputs may be respent. */
     bool AbandonTransaction(const uint256& hashTx);
 
+    //! Verify wallet naming and perform salvage on the wallet if required
+    static bool Verify(std::string wallet_file, bool salvage_wallet, std::string& error_string, std::string& warning_string);
+
     /* Initializes the wallet, returns a new CWallet instance or a null pointer in case of an error */
     static CWallet* CreateWalletFromFile(const std::string& name, const fs::path& path);
 
@@ -1268,7 +1269,7 @@ public:
      * Wallet post-init setup
      * Gives the wallet a chance to register repetitive tasks and complete post-init tasks
      */
-    void postInitProcess(CScheduler& scheduler);
+    void postInitProcess();
 
     /* AutoBackup functionality */
     static bool InitAutoBackup();
