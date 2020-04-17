7ce895c24413377d31170ba4c0e7468046c757ee
pasta <pasta@dashboost.org>
2020-04-16 20:17:25 -0500

    13017 continued
    Signed-off-by: pasta <pasta@dashboost.org>

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -29,11 +29,6 @@
 #include <QMessageBox>
 #include <QTimer>
 
-#ifdef ENABLE_WALLET
-typedef CWallet* CWalletRef;
-extern std::vector<CWalletRef> vpwallets;
-#endif //ENABLE_WALLET
-
 OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
     QDialog(parent),
     ui(new Ui::OptionsDialog),
@@ -263,8 +258,8 @@ void OptionsDialog::on_okButton_clicked()
     mapper->submit();
 #ifdef ENABLE_WALLET
     privateSendClient.nCachedNumBlocks = std::numeric_limits<int>::max();
-    if(!vpwallets.empty())
-        vpwallets[0]->MarkDirty();
+    if(HasWallets())
+        GetWallets()[0]->MarkDirty();
 #endif // ENABLE_WALLET
     accept();
     updateDefaultProxyNets();
