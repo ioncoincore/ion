2bdf58ad9d434c22b60379549ea15c7a1231c7e3
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-06-11 11:39:04 +0300

    Some Dashification (#3513)
    * Trivial Dashification
    
    * Tweak getnetworkinfo and dumpwallet help text
    
    We don't have RBF and Segwit
    
    * CopyrightHolders should also check for missing "Dash Core" copyright

--- contrib/devtools/symbol-check.py
+++ contrib/devtools/symbol-check.py
@@ -53,7 +53,7 @@ READELF_CMD = os.getenv('READELF', '/usr/bin/readelf')
 CPPFILT_CMD = os.getenv('CPPFILT', '/usr/bin/c++filt')
 # Allowed NEEDED libraries
 ALLOWED_LIBRARIES = {
-# bitcoind and bitcoin-qt
+# dashd and dash-qt
 'libgcc_s.so.1', # GCC base support
 'libc.so.6', # C library
 'libpthread.so.0', # threading
@@ -63,12 +63,7 @@ ALLOWED_LIBRARIES = {
 'libatomic.so.1',
 'ld-linux-x86-64.so.2', # 64-bit dynamic linker
 'ld-linux.so.2', # 32-bit dynamic linker
-'ld-linux-aarch64.so.1', # 64-bit ARM dynamic linker
-'ld-linux-armhf.so.3', # 32-bit ARM dynamic linker
-'ld-linux-riscv64-lp64d.so.1', # 64-bit RISC-V dynamic linker
-'ld64.so.2', # For powerpc64le
-'ld64.so.1', # For S390x
-# bitcoin-qt only
+# dash-qt only
 'libX11-xcb.so.1', # part of X11
 'libX11.so.6', # part of X11
 'libxcb.so.1', # part of X11
