Bitcoin-ABC/bitcoin-abc

Merge #11351: Refactor: Modernize disallowed copy constructors/assignment
Summary:
2a07f878a Refactor: Modernize disallowed copy constructors/assignment (Dan Raviv)

Pull request description:

  Use C++11's better capability of expressing an interface of a non-copyable class by publicly deleting its copy ctor and assignment operator instead of just declaring them private.

Tree-SHA512: 878f446be5a136bb2a90643aaeaca62948b575e6ef71ccc5b4b8f373e66f36ced00665128f36504e0ccfee639863d969329c4276154ef9f2a9de9137f0801e01

Backport of Core PR11351
https://github.com/bitcoin/bitcoin/pull/11351

Test Plan:
make check
test_runner.py

Reviewers: jasonbcox, deadalnix, Fabien, O1 Bitcoin ABC, #bitcoin_abc, markblundeberg

Reviewed By: O1 Bitcoin ABC, #bitcoin_abc, markblundeberg

Subscribers: markblundeberg

Differential Revision: https://reviews.bitcoinabc.org/D2983

src/streams.h
@@ -572,10 +572,6 @@ public:
  */
 class CAutoFile {
 private:
-    // Disallow copies
-    CAutoFile(const CAutoFile &);
-    CAutoFile &operator=(const CAutoFile &);
-
     const int nType;
     const int nVersion;
 
@@ -588,6 +584,10 @@ public:
     }
 
     ~CAutoFile() { fclose(); }
+
+    // Disallow copies
+    CAutoFile(const CAutoFile &) = delete;
+    CAutoFile &operator=(const CAutoFile &) = delete;
 
     void fclose() {
         if (file) {
@@ -686,10 +686,6 @@ public:
  */
 class CBufferedFile {
 private:
-    // Disallow copies
-    CBufferedFile(const CBufferedFile &);
-    CBufferedFile &operator=(const CBufferedFile &);
-
     const int nType;
     const int nVersion;
 
@@ -734,6 +730,10 @@ public:
     }
 
     ~CBufferedFile() { fclose(); }
+
+    // Disallow copies
+    CBufferedFile(const CBufferedFile &) = delete;
+    CBufferedFile &operator=(const CBufferedFile &) = delete;
 
     int GetVersion() const { return nVersion; }
     int GetType() const { return nType; }

ioncoincore/ion

src/streams.h
@@ -419,10 +419,6 @@ public:
 class CAutoFile
 {
 private:
-    // Disallow copies
-    CAutoFile(const CAutoFile&);
-    CAutoFile& operator=(const CAutoFile&);
-
     const int nType;
     const int nVersion;
 
@@ -438,6 +434,10 @@ public:
     {
         fclose();
     }
+
+    // Disallow copies
+    CAutoFile(const CAutoFile&) = delete;
+    CAutoFile& operator=(const CAutoFile&) = delete;
 
     void fclose()
     {
@@ -528,10 +528,6 @@ public:
 class CBufferedFile
 {
 private:
-    // Disallow copies
-    CBufferedFile(const CBufferedFile&);
-    CBufferedFile& operator=(const CBufferedFile&);
-
     const int nType;
     const int nVersion;
 
@@ -572,6 +568,10 @@ public:
     {
         fclose();
     }
+
+    // Disallow copies
+    CBufferedFile(const CBufferedFile&) = delete;
+    CBufferedFile& operator=(const CBufferedFile&) = delete;
 
     int GetVersion() const { return nVersion; }
     int GetType() const { return nType; }